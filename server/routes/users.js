61f1bca0ffee4f4a734480cb20834546dcc71b59
Sarujan P <saru.contacts@gmail.com>
2023-08-17 19:51:56 +0530

    payment routes added to users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,15 @@
 import express from "express"
-import { createUser, loginUser, getProfile, updateProfile, getTrips, getPlanets, tripRequest, getPackages, searchPlanets, verifyEntryPass, verifyToken, refreshToken, logout} from "../controllers/users.js"
+import { createUser, loginUser, getProfile, updateProfile, getTrips, getPlanets, tripRequest, cancelPayment, payHere, getPackages, searchPlanets, verifyEntryPass, verifyToken, refreshToken, logout} from "../controllers/users.js"
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/profile", verifyToken, refreshToken, getProfile);
-router.put("/profile/:id", verifyToken, refreshToken, updateProfile);
+router.put("/profile", verifyToken, refreshToken, updateProfile);
 router.get("/trips", verifyToken, refreshToken, getTrips);
+router.get("/payhere/callback", verifyToken, refreshToken, payHere);
+router.get("/payhere/cancel", verifyToken, refreshToken, cancelPayment);
 router.get("/planets", verifyToken, refreshToken, getPlanets);
 router.get("/packages", verifyToken, refreshToken, getPackages);
 router.post("/request", verifyToken, refreshToken, tripRequest);
