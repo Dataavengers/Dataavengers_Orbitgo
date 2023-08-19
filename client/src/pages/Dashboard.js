90bda73c1262d94a4d29a36cc5ca14103d000e79
lakindu <64328828+lakindu-banneheka@users.noreply.github.com>
2023-08-19 08:42:37 +0530

    dashboard responsiveness

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -20,29 +20,31 @@ const Dashboard = () => {
 
     return (
         <Layout>
-            <div className='text-white font-inter text-[1.25rem] font-bold text-left' >
-                WELCOME BACK!!!
-            </div>
-            <div className='w-full text-white flex flex-col justify-center items-center ' >
-                <div 
-                className='flex absolute top-[21%] left-[50%]' >
-                    <div className='' >
-                        <LocationPinIcon />
+            <div className='flex flex-col justify-between items-center w-full p-0 h-[90%] space-y-0 md:w-[80%]' >
+                <div className='text-white font-inter text-[1.25rem] font-bold text-left' >
+                    WELCOME BACK!!!
+                </div>
+                <div className='w-full text-white flex flex-col justify-center items-center ' >
+                    <div 
+                    className='flex absolute top-[21%] left-[50%]' >
+                        <div className='' >
+                            <LocationPinIcon />
+                        </div>
+                        <div className='text-white font-inter text-[0.75rem] font-medium' >You are here!</div>
                     </div>
-                    <div className='text-white font-inter text-[0.75rem] font-medium' >You are here!</div>
+                    <img className='w-[90%]  lg:w-[60%]' src={worldImage} alt='world-img'/>
+                    <div className='flex justify-end w-full pt-5'>
+                        <button onClick={onClickTrunBackButton} >
+                            <BackIcon />
+                        </button>
+                    </div>
                 </div>
-                <img className='w-full' src={worldImage} alt='world-img'/>
-                <div className='flex justify-end w-full pt-5'>
-                    <button onClick={onClickTrunBackButton} >
-                        <BackIcon />
-                    </button>
+                <div className='w-full lg:w-[80%]' >
+                    <Button 
+                        btnName={'LETS GO!'}
+                        onClickButton={onClickLestGoButton}
+                    />
                 </div>
-            </div>
-            <div className='w-full ' >
-                <Button 
-                    btnName={'LETS GO!'}
-                    onClickButton={onClickLestGoButton}
-                />
             </div>
         </Layout>
     );
